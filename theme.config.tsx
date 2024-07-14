1d94ba03e5ae052e0b3c2104cc03ad38e287e99d
Alexis Badel <private@dotcore-lab.net>
2024-07-14 20:23:02 +0200

    upd(wiki): meta stuff

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import mainLogo from './public/banner.png'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span>Holtyx Games</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Holtyx Wiki" />
+      <meta property="og:description" content="Official knowledgebase of Holtyx Games community, powered by and for its members." />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    </>
+  ),
+  logo: (
+    <>
+      <Image alt="Holtyx Games logo" src={ mainLogo } />
+    </>
+  ),
   project: {
     link: 'https://github.com/dotCore-off/holtyx-wiki',
   },
